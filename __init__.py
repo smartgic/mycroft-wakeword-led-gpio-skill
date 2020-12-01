c9cc0f01e04757fee99da2928e7410b078ae7cbb
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-11-30 20:15:19 -0500

    [init] Little refact

--- __init__.py
+++ __init__.py
@@ -8,15 +8,26 @@ class WakeWordLedGpio(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    def on_settings_changed(self):
-        self.pin_mode = self.settings.get('pin_mode', 'board').upper()
-        self.pin_number = self.settings.get('pin_number', 10)
-        self.log.info('PIN mode set to {}'.format(self.pin_mode))
-        self.log.info('LED PIN number set to {}'.format(self.pin_number))
+    def _setup(self):
+        if self.settings is not None:
+            self.pin_mode = self.settings.get('pin_mode', 'board').upper()
+            self.pin_number = self.settings.get('pin_number')
+
+            if not self.pin_number:
+                self.speak_dialog('error.setup', data={
+                    "field": "pin"})
+                return
+
+            self.log.info('PIN mode set to {}'.format(self.pin_mode))
+            self.log.info('LED PIN number set to {}'.format(self.pin_number))
+
+    def on_websettings_changed(self):
+        self.log.debug('Applying the new configuration')
+        self._setup()
 
     def initialize(self):
-        self.settings_change_callback = self.on_settings_changed
-        self.on_settings_changed()
+        self.settings_change_callback = self.on_websettings_changed
+        self._setup()
 
         try:
             # Setup GPIO
@@ -26,18 +37,18 @@ class WakeWordLedGpio(MycroftSkill):
 
             # Catch event
             self.add_event('recognizer_loop:record_begin',
-                           self.handle_listener_started)
+                           self._handle_listener_started)
             self.add_event('recognizer_loop:record_end',
-                           self.handle_listener_ended)
+                           self._handle_listener_ended)
         except Exception:
             GPIO.cleanup()
             self.log.error('Cannot initialize GPIO - skill will not load')
             self.speak_dialog('error.initialize')
 
-    def handle_listener_started(self, message):
+    def _handle_listener_started(self, message):
         GPIO.output(self.pin_number, GPIO.LOW)
 
-    def handle_listener_ended(self, message):
+    def _handle_listener_ended(self, message):
         GPIO.output(self.pin_number, GPIO.HIGH)
 
 
